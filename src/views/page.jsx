cf14c0f863aa5a385a5a9d295995599463fb2890
Casey O'Keefe <the0surfer@aol.com>
2017-08-28 20:33:10 -0500

    finished

--- src/views/page.jsx
+++ src/views/page.jsx
@@ -1,25 +1,25 @@
 'use strict';
 
 class Page extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
   }
 
-  render() {
+  render () {
     return (
-      <div className="container">
-        <div className="main-img">
+      <div className='container'>
+        <div className='main-img'>
           <h1>Lorem Ipsum Article</h1>
         </div>
-        <div className="row">
-          <div className="col">
+        <div className='row'>
+          <div className='col'>
             <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo donec enim diam vulputate ut pharetra sit. Nunc congue nisi vitae suscipit tellus. Orci sagittis eu volutpat odio. Eget aliquet nibh praesent tristique magna. Id cursus metus aliquam eleifend mi in nulla. Nulla facilisi etiam dignissim diam quis. Diam in arcu cursus euismod quis viverra nibh cras. Sit amet nisl suscipit adipiscing. Velit laoreet id donec ultrices tincidunt arcu non sodales. Ultricies mi eget mauris pharetra et ultrices neque ornare. Facilisis mauris sit amet massa vitae tortor condimentum. Justo laoreet sit amet cursus. Quam vulputate dignissim suspendisse in est ante in. Ultrices in iaculis nunc sed augue. In pellentesque massa placerat duis ultricies lacus sed. Sodales ut eu sem integer vitae justo. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Mattis pellentesque id nibh tortor id. Egestas egestas fringilla phasellus faucibus scelerisque eleifend.</span>
           </div>
-          <div className="col">
-            <ReactForm />
+          <div className='col'>
+            <ReactForm color="beige"/>
             <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo donec enim diam vulputate ut pharetra sit. Nunc congue nisi vitae suscipit tellus. Orci sagittis eu volutpat odio. Eget aliquet nibh praesent tristique magna. Id cursus metus aliquam eleifend mi in nulla. Nulla facilisi etiam dignissim diam quis. Diam in arcu cursus euismod quis viverra nibh cras. Sit amet nisl suscipit adipiscing. Velit laoreet id donec ultrices tincidunt arcu non sodales.</span>
           </div>
-          <div className="col">
+          <div className='col'>
             <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo donec enim diam vulputate ut pharetra sit. Nunc congue nisi vitae suscipit tellus. Orci sagittis eu volutpat odio. Eget aliquet nibh praesent tristique magna. Id cursus metus aliquam eleifend mi in nulla. Nulla facilisi etiam dignissim diam quis. Diam in arcu cursus euismod quis viverra nibh cras. Sit amet nisl suscipit adipiscing. Velit laoreet id donec ultrices tincidunt arcu non sodales. Ultricies mi eget mauris pharetra et ultrices neque ornare. Facilisis mauris sit amet massa vitae tortor condimentum. Justo laoreet sit amet cursus. Quam vulputate dignissim suspendisse in est ante in. Ultrices in iaculis nunc sed augue. In pellentesque massa placerat duis ultricies lacus sed. Sodales ut eu sem integer vitae justo. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Mattis pellentesque id nibh tortor id. Egestas egestas fringilla phasellus faucibus scelerisque eleifend.</span>
           </div>
         </div>
@@ -29,22 +29,38 @@ class Page extends React.Component {
 }
 
 class ReactForm extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
+    this.state = {
+      submitted: false
+    };
+    this.submit = this.submit.bind(this);
   }
 
-  render() {
-    return (
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" />
-        <label htmlFor="name">Age:</label>
-        <input type="text" name="age" />
-        <label htmlFor="email">Email:</label>
-        <input type="text" name="email" />
-        <button type="submit" onClick={this.submit}>Submit</button>
-      </form>
-    );
+  render () {
+    if (!this.state.submitted) {
+      console.log(this.state.submitted);
+      return (
+        <form className={this.props.color}>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' />
+          <label htmlFor='name'>Age:</label>
+          <input type='text' name='age' />
+          <label htmlFor='email'>Email:</label>
+          <input type='text' name='email' />
+          <button type='submit' className='btn btn-danger' onClick={this.submit}>Submit</button>
+        </form>
+      );
+    } else {
+      return (
+        <h1> Thanks for submitting </h1>
+      );
+    }
+  }
+  submit () {
+    this.setState((state) => {
+      state.submitted = !state.submitted;
+    });
   }
 }
 
